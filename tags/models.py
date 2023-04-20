40e1582a4bf5d31e32ff76b7ea8bb431465d4f16
Lewey <dlmelchor12@gmail.com>
2023-04-20 10:51:42 -0700

    added tag model to the tags app
    allows users to tag a recipe with a short description like "yum"

--- tags/models.py
+++ tags/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from recipes.models import Recipe
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    recipes = models.ManyToManyField(Recipe, related_name="tags")
+
+    def __str__(self):
+        return self.name
