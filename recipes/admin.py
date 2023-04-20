34cb000e45c349730dff144a481d8888c23b5681
Lewey <dlmelchor12@gmail.com>
2023-04-19 21:19:55 -0700

    built a model to create recipes added to django admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-
+from .models import Recipe_model
 
 # Register your models here.
+admin.site.register(Recipe_model)
--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 
 # Create your models here.
+class Recipe_model(models.Model):
+    title = models.CharField(max_length=125)
+    ingredients = models.TextField()
+    recipe = models.TextField()
+    notes = models.TextField()
+    image = models.URLField(null=True, blank=True, max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
