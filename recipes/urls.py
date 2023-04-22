4103987e32c51f55a97b39c42048a32ce04ccc6c
Lewey <dlmelchor12@gmail.com>
2023-04-21 23:37:04 -0700

    replaced show recipe function with a class based view
    added a new value to the context

--- recipes/urls.py
+++ recipes/urls.py
@@ -14,7 +14,7 @@ from recipes.views import (
     create_recipe,
     change_recipe,
     log_rating,
-    show_recipe,
+    RecipeDetailView,
     RecipeListView,
 )
 
@@ -30,7 +30,7 @@ from recipes.views import (
 # URL PATTERNS FOR CLASS VIEWS
 urlpatterns = [
     path("", RecipeListView.as_view(), name="recipes_list"),
-    path("<int:pk>/", show_recipe, name="recipe_detail"),
+    path("<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("new/", create_recipe, name="recipe_new"),
     path("edit/", change_recipe, name="recipe_edit"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
