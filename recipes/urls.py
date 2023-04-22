faa1d5648e13f46e09277bf8546c7bd6892f5c4c
Lewey <dlmelchor12@gmail.com>
2023-04-22 00:28:20 -0700

    added a delete class based view
    allows users to delete recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -13,9 +13,10 @@ from django.urls import path
 from recipes.views import (
     RecipeCreateView,
     RecipeUpdateView,
-    log_rating,
+    RecipeDeleteView,
     RecipeDetailView,
     RecipeListView,
+    log_rating,
 )
 
 # URL PATTERNS FOR FUNCTION VIEWS
@@ -34,4 +35,5 @@ urlpatterns = [
     path("new/", RecipeCreateView.as_view(), name="recipe_new"),
     path("<int:pk>/edit/", RecipeUpdateView.as_view(), name="recipe_edit"),
     path("<int:recipe_id>/ratings/", log_rating, name="recipe_rating"),
+    path("<int:pk>/delete/", RecipeDeleteView.as_view(), name="recipe_delete"),
 ]
