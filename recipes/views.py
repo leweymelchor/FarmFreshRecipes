aab52c3d9c304c9d8242d9f8bbdc00207a61ab1b
Lewey <dlmelchor12@gmail.com>
2023-04-22 00:15:37 -0700

    created an edit class based view function
    allows users to edit recipes

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 
 from recipes.forms import RatingForm
@@ -40,23 +40,29 @@ class RecipeCreateView(CreateView):
     success_url = reverse_lazy("recipes_list")
 
 
-def change_recipe(request, pk):
-    if Recipe and RecipeForm:
-        instance = Recipe.objects.get(pk=pk)
-        if request.method == "POST":
-            form = RecipeForm(request.POST, instance=instance)
-            if form.is_valid():
-                form.save()
-                return redirect("recipe_detail", pk=pk)
-        else:
-            form = RecipeForm(instance=instance)
-    else:
-        form = None
-    context = {
-        "form": form,
-    }
-    return render(request, "recipes/edit.html", context)
+# def change_recipe(request, pk):
+#     if Recipe and RecipeForm:
+#         instance = Recipe.objects.get(pk=pk)
+#         if request.method == "POST":
+#             form = RecipeForm(request.POST, instance=instance)
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect("recipe_detail", pk=pk)
+#         else:
+#             form = RecipeForm(instance=instance)
+#     else:
+#         form = None
+#     context = {
+#         "form": form,
+#     }
+#     return render(request, "recipes/edit.html", context)
 
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    template_name = "recipes/edit.html"
+    fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
 
 # def show_recipes(request):
 #     context = {
