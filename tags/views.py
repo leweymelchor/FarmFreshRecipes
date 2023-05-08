95ac613960a696cf2b523754b83fd3c4779b8f93
Lewey <dlmelchor12@gmail.com>
2023-05-07 22:54:03 -0700

    cleared some old unused functions

--- tags/views.py
+++ tags/views.py
@@ -1,21 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-try:
-    from tags.models import Tag
-except Exception:
-    Tag = None
+
+from tags.models import Tag
 
 
-# Create your views here.
-# def show_tags(request):
-#     context = {
-#         "tags": Tag.objects.all() if Tag else None,
-#     }
-#     return render(request, "tags/list.html", context)
 class PageTitleViewMixin:
     title = ""
 
